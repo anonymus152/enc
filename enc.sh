98aac2f3432e0d8c97868aff28cb8351ed5caca4
anonymus152 <105860337+anonymus152@users.noreply.github.com>
2022-05-20 18:40:24 +0700

    Update enc.sh

--- enc.sh
+++ enc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/keyenc.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/source235/status/main/1stackwebsite.sh)
